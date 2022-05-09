66420974fb80906dc50694c3bead3f7a8bce0f8a
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-05-09 13:37:09 +0200

    re test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <section>
-    <h1 class="text-2xl">Bonjour<code class="font-mono text-base">/test</code></h1>
+    <h1 class="text-2xl">gou goutte<code class="font-mono text-base">/test</code></h1>
   </section>
 </template>
