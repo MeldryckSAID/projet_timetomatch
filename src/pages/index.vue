3849c163798127516f81beab6b209f32e597dfc0
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-05-10 16:59:44 +0200

    test final

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
-    <h1 class="text-2xl">david test pour ilkay  goutte<code class="font-mono text-base">/test</code></h1>
-  </section>
+    <h1 class=" border-l-indigo-500  text-4xl">david test pour ilkay  goutte<code class="font-mono text-base">/test</code></h1>
+ <h3 class="text-6xl ">jsuis tro fort</h3>
+ </section>
 </template>
